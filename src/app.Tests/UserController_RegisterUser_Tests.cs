e680658c62ca2e98f993fd0964630f1e5bd14d96
Maciej Aniserowicz <maciej.aniserowicz@gmail.com>
2014-03-31 23:29:54 +0200

    added test for UserController - verify that it throws when email is invalid

---
+++ src/app.Tests/UserController_RegisterUser_Tests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using NSubstitute;
+using Procent.DependencyInjection.app;
+using Xunit;
+
+namespace Procent.DependencyInjection.Tests
+{
+    public class UserController_RegisterUser_Tests
+    {
+        readonly UsersController _controller;
+        readonly IEmailValidator _emailValidator;
+        string _email;
+
+        public UserController_RegisterUser_Tests()
+        {
+            _emailValidator = Substitute.For<IEmailValidator>();
+            var linkGenerator = Substitute.For<IActivationLinkGenerator>();
+
+            _controller = new UsersController(_emailValidator, linkGenerator);
+
+            _email = "email";
+        }
+
+        void execute()
+        {
+            _controller.RegisterUser(_email);
+        }
+
+        [Fact]
+        public void throws_when_email_not_valid()
+        {
+            _emailValidator.Validate(_email).Returns(false);
+
+            Assert.Throws<ArgumentException>(
+                () => execute()
+            );
+        }
+    }
+}
