8ec29e4c10fedef9790c3126e580c6a680888520
Maciej Aniserowicz <maciej.aniserowicz@gmail.com>
2014-03-31 22:54:21 +0200

    fixed email validator

--- src/app/EmailValidator.cs
+++ src/app/EmailValidator.cs
@@ -4,7 +4,7 @@ namespace Procent.DependencyInjection.app
 {
     public class EmailValidator
     {
-        const string EMAIL_REGEX = @"[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}";
+        const string EMAIL_REGEX = @"[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}";
 
         public bool Validate(string email)
         {
